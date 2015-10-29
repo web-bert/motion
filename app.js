06106c92021c2fbcb89aa655ef2fea6d865a1b58
Graham Veal <grahamveal@Grahams-iMac.local>
2015-10-29 21:06:09 +0000

    Added test with sensor

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log( 'test' );
-process.exit();
+console.log( 'Starting' );
+
+var onoff = requite( 'onoff' );
+var Gpio = onoff.Gpio;
+var sensor = new Gpio( 26, 'in' );
+
+sensor.watch( function( err, state ){
+	console.log( 'Sensor state change: ' + state );
+} );
+
+
+//process.exit();
