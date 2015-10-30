adb7855ca5906409f32de619efbee55ac255f326
Graham Veal <grahamveal@gmail.com>
2015-10-30 19:44:13 +0000

    Added http call to an API to play audio. Added require of config.json for config items

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 console.log( 'Starting' );
 
 var onoff = require( 'onoff' );
+var http = require( 'http' );
+var config = require( './config' );
+
 var Gpio = onoff.Gpio;
-var sensor = new Gpio( 7, 'in', 'both' );
+var sensor = new Gpio( config.sensor.gpio, 'in', 'both' );
 
 function exit(){
 	sensor.unexport();
@@ -11,13 +14,44 @@ function exit(){
 
 sensor.watch( function( err, state ){
 
+	var path;
+	var req;
+
 	if( err ){
 
 		console.log( 'Error with sensor ', err );
 
 	} else {
 
-		console.log( 'Sensor state change: ' + state );
+		if( state === 1 ){
+
+			console.log( 'Sending request to /play/' );
+
+			var req = http.request( {
+				hostname: config.api.host,
+				port: config.api.port,
+				path: '/play/'
+			}, function( res ){
+	
+				var data = '';
+
+				res.on( 'data', function( chunk ){
+					data += chunk;
+				} );
+			
+				res.on( 'end', function(){
+					console.log( 'Response received. Status: %s, body: %s', res.statusCode, data );
+				} );
+			} );
+
+			req.on( 'error', function( e ){
+				console.log( 'Error with request: %s', e.message );
+			} );
+
+			req.end();
+		}
+
+		//console.log( 'Sensor state change: ' + state );
 	}
 } );
 
