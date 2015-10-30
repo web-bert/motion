1952f24f57ddf9373e26dc0d83bba0013b402c3f
Graham Veal <grahamveal@gmail.com>
2015-10-30 16:51:07 +0000

    First working version

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 console.log( 'Starting' );
 
-var onoff = requite( 'onoff' );
+var onoff = require( 'onoff' );
 var Gpio = onoff.Gpio;
-var sensor = new Gpio( 26, 'in' );
+var sensor = new Gpio( 7, 'in', 'both' );
+
+function exit(){
+	sensor.unexport();
+	process.exit();
+}
 
 sensor.watch( function( err, state ){
-	console.log( 'Sensor state change: ' + state );
+
+	if( err ){
+
+		console.log( 'Error with sensor ', err );
+
+	} else {
+
+		console.log( 'Sensor state change: ' + state );
+	}
 } );
 
+console.log( 'Waiting for sensor state changes...' );
 
-//process.exit();
+process.on( 'SIGINT', exit );
